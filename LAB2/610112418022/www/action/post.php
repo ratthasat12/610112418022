198f10971bddf1033ccf27a0ca8ed5c2ff08f7c7
ratthasat12 <ratthasatop@gmail.com>
2020-01-17 10:34:09 +0700

    LAB1

---
+++ LAB2/610112418022/www/action/post.php
@@ -0,0 +1,19 @@
+<?php
+
+$sport =  $_POST['sl_sport']?$_POST['sl_sport']:$_GET['getSport'];
+
+cal_sport($sport);
+
+function cal_sport($sport){
+	switch ($sport){
+		case "1" :
+			$sport = "Football";
+			break;
+		case "2" :
+			$sport = "Tenis";
+			break;
+		default:
+			$sport = "Out of type.";
+	}
+	echo $sport;
+};
